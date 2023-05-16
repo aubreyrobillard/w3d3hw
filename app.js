4cf7e36c6751e3a80adb2eb3010ab8e43602c306
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-15 21:55:31 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -4,11 +4,29 @@
 // You will make things appear and disappear (and more) with the magic of jQuery!
 // This homework is recommended to be done with a Harry Potter theme. But if you are unfamiliar with Harry Potter, that's cool, you can make up all your wizard details.
 
-console.log(" ============ YEAR 1 ===============")
-
-$(() => {
-	const $container = $("body").append($('<div>').attr('id', 'container').append($('<h1>').text("Hogwarts")))
+//============ YEAR 1 ===============
+// $(() => {
+// 	const $container = $("body").append($('<div>').attr('id', 'container').append($('<h1>').text("Hogwarts")))
     // console.log($container)
-    // console.log(document)
-});
+    //console.log(document)
+// });
 
+
+//============ YEAR 2 ===============
+$(() => {
+	$("body").append($('<div>').attr('id', 'container'))
+    
+    let $container = $("#container")
+    
+    $container.append($('<h1>').text("Hogwarts"))
+    // console.log($container)
+    console.log(document)
+
+    const $img = "https://i.pinimg.com/originals/41/8f/9f/418f9ffb3c71bea43dab323e3306adc4.jpg"
+
+    $container.append($('<h2>').text("Aubrey A Robillard"))
+    $container.append($('<h3>').text("Slytherin"))
+    $container.append($('<h4>').attr('class', 'dog').text("Koa Boa"))
+    $container.append($('<h4>').text("Holly Wand with Unicorn Hair Core")).append($("<img>").attr('src', $img))
+    $container.append($('<h5>').text("Your wand is made from rarely used Holly wood, with a core of Unicorn Hair. This type of wand bonds deeply with its first owner, sacrificing some power for reliability and consistency. This probably means that you're a very dependable person. Users are rarely seen to defect to the dark side, which has led to some witches and wizards thinking of Unicorn Hair wands as a symbol of the light."))
+});
