46c19a91c6898f344406504cacf85425f8adbf36
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-15 20:08:42 -0500

    repo set up

---
+++ app.js
@@ -0,0 +1,11 @@
+// jQuery Magic
+// Intro
+// You are now a wizard and you will no longer hard code html during your time at Hogwarts (or the duration of this assignment)
+// You will make things appear and disappear (and more) with the magic of jQuery!
+// This homework is recommended to be done with a Harry Potter theme. But if you are unfamiliar with Harry Potter, that's cool, you can make up all your wizard details.
+
+if (typeof jQuery == 'undefined'){
+    console.log('oops! I still have to link my jQuery properly!');
+  } else {console.log('I did it! I linked jQuery and this js file!')};
+
+  
