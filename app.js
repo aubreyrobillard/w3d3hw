2ef3330bd15ea1b0e6cebb289e7a51752cf3c069
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-15 20:43:40 -0500

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -4,5 +4,11 @@
 // You will make things appear and disappear (and more) with the magic of jQuery!
 // This homework is recommended to be done with a Harry Potter theme. But if you are unfamiliar with Harry Potter, that's cool, you can make up all your wizard details.
 
+console.log(" ============ YEAR 1 ===============")
 
+$(() => {
+	const $container = $("body").append($('<div>').attr('id', 'container').append($('<h1>').text("Hogwarts")))
+    // console.log($container)
+    // console.log(document)
+});
 
