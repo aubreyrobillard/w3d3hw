1331d8cf3f059fdcd5b8bc97390dba730246a984
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-17 22:51:56 -0500

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- app.js
+++ app.js
@@ -17,10 +17,14 @@ $(() => {
     const $img = "https://i.pinimg.com/originals/41/8f/9f/418f9ffb3c71bea43dab323e3306adc4.jpg"
 
     $container.append($('<h2>').text("Aubrey A Robillard"))
+    
     $container.append($('<h3>').text("Slytherin"))
+    
     $container.append($('<h4>').attr('class', 'dog').text("Koa Boa"))
+    
     $container.append($('<h4>').text("Holly Wand with Unicorn Hair Core")).append($("<img>").attr('src', $img))
-    $container.append($('<h5>').text("Your wand is made from rarely used Holly wood, with a core of Unicorn Hair. This type of wand bonds deeply with its first owner, sacrificing some power for reliability and consistency. This probably means that you're a very dependable person. Users are rarely seen to defect to the dark side, which has led to some witches and wizards thinking of Unicorn Hair wands as a symbol of the light."))
+    
+        $container.append($('<h5>').text("Your wand is made from rarely used Holly wood, with a core of Unicorn Hair. This type of wand bonds deeply with its first owner, sacrificing some power for reliability and consistency. This probably means that you're a very dependable person. Users are rarely seen to defect to the dark side, which has led to some witches and wizards thinking of Unicorn Hair wands as a symbol of the light."))
 
     const $ul = $('<ul>')
     
@@ -32,4 +36,45 @@ $(() => {
         $ul.append($('<li>').addClass("secret").text("Time Turner"))
         $ul.append($('<li>').addClass("dog").text("Leash"))
         $ul.append($('<li>').text("Bertie Bott's Every Flavor Jelly Beans"))
+
+
+   {
+     $container.append($("<h5>").addClass("title").text("Spring 2017"))
+    
+    // Add a table
+    
+    let $table = ($('<table>'))
+    let $thead = ($('<thead>'))
+    let $tbody = ($('<tbody>'))
+    
+    $container.append($table)
+        $table.append($($thead).append($("<th>").text("Day")).append($("<th>").text("Classes")))
+        
+        $table.append($tbody).append($('<tr>').append($('<td>').text("Monday")).append($('<td>').text("Herbology, Tranfiguration, Potions")))
+        
+        $table.append($tbody).append($('<tr>').append($('<td>').text("Tuesday")).append($('<td>').text("Charms, Histroy of Magic, Quidditch Practice")))
+        
+        $table.append($tbody).append($('<tr>').append($('<td>').text("Wednesday")).append($('<td>').text("Herbology, Deffense Aginast the Dark Arts, Quidditch Practice")))
+        
+        $table.append($tbody).append($('<tr>').append($('<td>').text("Thursday")).append($('<td>').text("Charms, Tranfiguration, Quidditch Practice")))
+        
+        $table.append($tbody).append($('<tr>').append($('<td>').text("Friday")).append($('<td>').text("Potions, History of Magic, Defense Against the Dark Arts")))
+   }
 });
+
+
+
+
+//<table>
+    //<thead>
+        //<tr>
+            //<th colspan="2">The table header</th>
+        //</tr>
+    //</thead>
+    //<tbody>
+        //<tr>
+            //<td>The table body</td>
+            //<td>with two columns</td>
+        //</tr>
+    //</tbody>
+//</table>
